8d0f19ff6eb943f482c98a996eba3b5d3d6b1dd1
Prachi <prachic1210@gmail.com>
2022-01-23 18:06:17 -0500

    Simple Node server created

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
+//Simple Node js server
+var http = require('http');
+var server = http.createServer(function(req,res){
+    if((req.url == '/')){
+        res.writeHead(200, {'Content-type':'text/html'});
+        res.write('<html><body><p><strong>Hello World </strong></p></body></html>')
+    }
+})
+
+server.listen(5000);
+console.log('Server is listening at port 5000');
+
+//Simple hello world using Node
 var msg ='Hello World';
 console.log(msg);
