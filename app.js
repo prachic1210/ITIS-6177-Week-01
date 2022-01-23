05800c0551c16508a3def6e8e142bf066ce9cd07
Prachi <prachic1210@gmail.com>
2022-01-23 18:36:17 -0500

    Create a simple Express Node server

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-//Simple Node js server
-var http = require('http');
-var server = http.createServer(function(req,res){
-    if((req.url == '/')){
-        res.writeHead(200, {'Content-type':'text/html'});
-        res.write('<html><body><p><strong>Hello World </strong></p></body></html>')
-    }
+//Simple Node js server using Express
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
 })
 
-server.listen(5000);
-console.log('Server is listening at port 5000');
+app.listen(port,()=>{
+    console.log('Server is listening on port 5000...')
+})
+
+//Simple Node js server
+//var http = require('http');
+//var server = http.createServer(function(req,res){
+    //if((req.url == '/')){
+        //res.writeHead(200, {'Content-type':'text/html'});
+        //res.write('<html><body><p><strong>Hello World </strong></p></body></html>')
+    //}
+//})
+
+//server.listen(5000);
+//console.log('Server is listening at port 5000');
 
 //Simple hello world using Node
 var msg ='Hello World';
